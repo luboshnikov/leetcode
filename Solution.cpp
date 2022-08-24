a2909e3fb754ad0c9e6748af56a00ce46cea6688
luboshnikov <luboshnikovm@gmail.com>
2022-08-24 14:02:12 +0200

    isPowerOfThree

--- Solution.cpp
+++ Solution.cpp
@@ -521,5 +521,15 @@ public:
         }
         return false;
     }
+
+    static bool isPowerOfThree(int n) {
+        if(n < 1) {
+            return false;
+        }
+        while(n % 3 == 0){
+            n /= 3;
+        }
+        return n == 1;
+    }
 };
 

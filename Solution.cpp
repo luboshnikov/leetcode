56758774e1ff49920aac6f04f3e05b37a2cfab8c
luboshnikov <luboshnikovm@gmail.com>
2022-08-24 20:42:51 +0200

    sortedArrayToBST

--- Solution.cpp
+++ Solution.cpp
@@ -28,6 +28,16 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class MyCalendar {
 public:
 
@@ -530,6 +540,20 @@ public:
             n /= 3;
         }
         return n == 1;
+    }
+
+
+    static TreeNode* BST(vector<int>::iterator begin, vector<int>::iterator end){
+        if(end - begin == 0){
+            return nullptr;
+        } else if(end - begin == 1){
+            return new TreeNode(*begin);
+        }
+        auto mid = begin + (end - begin)/2;
+        return new TreeNode(*mid, BST(begin, mid), BST(mid+1, end));
+    }
+    static TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return BST(nums.begin(), nums.end());
     }
 };
 
